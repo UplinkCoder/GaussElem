ea66c6dbb88c290a598ae27aeff9f92684ab80b2
Uplink.Coder <uplink.coder@gmail.com>
2013-11-21 16:18:34 +0100

    modified build-script

--- build.ldc2.sh
+++ build.ldc2.sh
@@ -1,1 +1,2 @@
-ldc2 -Oz parseEquationSystem.d pegged/grammar.d pegged/peg.d pegged/parser.d pegged/dynamic/grammar.d pegged/dynamic/peg.d -oq
+ldc2 -Oz *.d pegged/grammar.d pegged/peg.d pegged/parser.d pegged/dynamic/grammar.d pegged/dynamic/peg.d -o- -oq
+
