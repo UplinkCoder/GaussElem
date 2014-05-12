bebee2246057c76bcad39fdd09a22c72bee5f098
UplinkCoder <UplinkCoder@users.noreply.github.com>
2014-05-12 11:08:44 +0200

    commiting new Variable struct

--- System.d
+++ System.d
@@ -8,51 +8,62 @@ struct System {
 		rows=r;
 		allIdentifiers = getAllIdentifiers(r);
 	}
+	getRowsWith (char[] varIds) {
+		Row[] res;
+		foreach(r;rows) {
+			if (var.Ids in r.getIdentifiers) {
+				res ~= r;
+			}
+		}
+	}
+	
 	string toString() {
 		string str;
 		foreach (row;rows) {
-			str ~= row.toString ~ '\n';
+			str ~= row.toString ~ "\n";
 		}
 		return str;
 	}
 }
 
 struct Row {
+	struct Variable {
+		char chr;
+		double val;
+		@property hasValue() {return (val != double.init);}
+	}
+	
 	 struct Pair {
-      int val;
-      char chr;
-  }
-  
-  alias  map!("a.chr") chrmap;
-  alias  map!("a.val") valmap;
-	 
+		double scalar;
+		Variable var;
+		deprecated alias val = scalar;
+	}
+
 	char[] identfiers;
 	Pair[] vars;
 	int res;
 	
 
-	
 	@disable this();
 	this(Pair[] pa,int result) {
-		writeln( map!("a.chr")(pa));
-		sort!("a.chr<b.chr")(pa); 
+		writeln( map!("a.var.chr")(pa));
+		sort!("a.var.chr<b.var.chr")(pa); 
 		vars=pa;
 		res = result;
 	}
 	
+
 	string toString() {
 		string str;
 		foreach (var;vars) {
 			if (var.val>0 && var !is vars[0]) str ~= '+';
-			str ~= to!string(var.val) ~ var.chr;
+			str ~= to!string(var.val) ~ var.var.chr;
 		}
 		str ~= '=' ~ to!string(res);   
 		
 		return str;
 	}
-	
-}
-
+}	
 private char[] getAllIdentifiers (Row[] parsedRows) {
 	char[] vars;
 		foreach (r;parsedRows) {
@@ -67,8 +78,8 @@ private char[] getAllIdentifiers (Row[] parsedRows) {
 private char[] getIdentfiers(Row r) {
 	char[] ids;
 		foreach (pair;r.vars) {
-				if(find(ids,pair.chr)==[])
-				ids ~= pair.chr;
+				if(find(ids,pair.var.chr)==[])
+				ids ~= pair.var.chr;
 		}
 	return ids;
 }
