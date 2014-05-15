e60c16d07f47ac994d97ed607f89d7ad67fe895c
uplink <root@proxmox.fritz.box>
2014-05-15 11:32:39 +0200

    added eliminateKownVariables

--- reduceMatrix.d
+++ reduceMatrix.d
@@ -1,4 +1,7 @@
 import aaSystem;
+alias AASystem.AARow AARow;
+debug import std.stdio;
+
 
 double[char] reduceSingles(AASystem sys/*,ref Variable[char] vars*/) {
 	auto svrs = sys.getSingleVariableRows;
@@ -9,15 +12,31 @@ double[char] reduceSingles(AASystem sys/*,ref Variable[char] vars*/) {
 	return res;
 } 
 
-double[char] reduce_(AASystem sys, double[char] results) {
-	auto rs = sys.getRowsWith(results.keys);
+auto reduce_(AASystem sys, double[char] results) {
+	AARow[] rs;
+	foreach (key;results.keys) {
+		rs ~= sys.getRowsWith([key]);
+	}
+	debug  writeln(rs);
 	double[char] res;
 	foreach (ref r;rs) {
-		//r = r.eliminateKnownVariables(results);
+		r = r.eliminateKnownVariables(results);
 	}
-	return AASystem(rs).reduceSingles;
+	return rs;
 }
 
-//AARow eliminateKnownVariables(AARow row, double[char] kwnvars) {
-	 
-  
+AARow eliminateKnownVariables(AARow row, double[char] kwnvars) {
+	if (row.singleVariable) return row;
+	AARow res;
+	res.res = row.res;
+	foreach(var;row.vals.keys) {
+		if(var !in kwnvars) {
+			res.vals[var] = row.vals[var];
+		} else {
+			res.res -= row.vals[var]*kwnvars[var];
+		}
+	}
+	return res;
+}
+
+
