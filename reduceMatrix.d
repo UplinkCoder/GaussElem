b55f41f4d624db95696492d1fc5d742329e9926c
Stefan K <uplink.coder@gmail.com>
2014-06-28 11:12:58 +0000

    FIXUP pure and other stuff

--- reduceMatrix.d
+++ reduceMatrix.d
@@ -12,7 +12,7 @@ import std.stdio;
 //AARow applyLCM (AARow row) {
 //
 //}
-AASystem applyFuncToEachRow(alias f)(AASystem sys)
+AASystem applyFuncToEachRow(alias f)(AASystem sys) pure
 if (is(typeof(&f):AARow function (AARow))||is(typeof(&f):AARow function(AARow,double))) {
 	foreach (ref row;sys.rows) {
 		row = f(row);
@@ -20,71 +20,60 @@ if (is(typeof(&f):AARow function (AARow))||is(typeof(&f):AARow function(AARow,do
 	return sys;
 }
 
-T lcm (T)(T a,T b) {
+T lcm (T)(T a,T b) pure {
 	return a/gcd(a,b) * b;
 }
-AARow mulByLCM (AARow row,double v=1) {
+AARow mulByLCM (AARow row, double v=1) pure {
 	auto multiplicator = reduce!((a,b) => lcm(a,b))(v,row.scalars);
 	return row*multiplicator;
 }
-AARow divByGCD (AARow row,double v=1) {
+AARow divByGCD (AARow row,double v=1) pure {
 		auto divisor = reduce!((a,b) => gcd(a,b))(v,row.scalars);
 		return row / divisor;
 }
-AASystem applyGCD (AASystem sys) {
+AASystem applyGCD (AASystem sys) pure {
 	return applyFuncToEachRow!divByGCD(sys);
 }
-AASystem applyLCM (AASystem sys) {
+AASystem applyLCM (AASystem sys) pure {
 	return applyFuncToEachRow!mulByLCM(sys);
 }
 
-AASystem eliminateKnownVariables(AASystem sys,in double[char] kwnvars) {
-	foreach (i,ref row;sys.rows) {
+AARow eliminateKnownVariables(AARow row,in double[char] kwnvars) {
 		if (!row.singleVariable) {
 			foreach(var;row.vars) {
 				if(var in kwnvars) {
-					row.res -= row[var]*sys.kwnvars[var]; // a=7;a+b=9;  a+b=9 / -a; b=2
+					row.res -= row[var]*kwnvars[var]; // a=7;a+b=9;  a+b=9 / -a; b=2
 					row.scalars.remove(var);
 				}
 			}
 		}
-	}
-	return sys;
+	return row;
 }
 
 			// eliminate if singleVariable
-AASystem eliminateSingels(AASystem sys,inout double[char] kwnvars) {
-	foreach (i,ref row;sys.rows) {
+AARow eliminateSingels(AARow row,ref double[char] kwnvars) {
 		if (row.singleVariable) {
 			foreach(var;row.vars) {			
 				auto ev = row.vars[0];
-
-				if (row.scalars.keys[0] in sys.kwnvars) {
+				if (row.scalars.keys[0] in kwnvars) {
 					assert(0,"something is wrong");
 					break;
 				}
 				assert( (row/row[ev])[ev] == 1, "something is very wrong");
-				sys.kwnvars[ev] = (row/row[ev]).res;  // 7a=14  | /7; a=2 | knwvars[a] = 2
-				sys.rows.remove(i);
-				sys.rows.length -= 1;
+				kwnvars[ev] = (row/row[ev]).res;  // 7a=14  | /7; a=2 | knwvars[a] = 2
 			}  	
 		}
-	}
-	return sys;
+	return row;
 }
 
 	// now we can try to reduce even more by subtracting rows form each other
-AASystem eliminateRowVar(AASystem sys,in double[char] kwnvars) {
-	if (sys.rows.length>1) {
-		foreach (i,row;sys.rows[1 .. $]) {
-			writeln("looking for intersctions in row 0 and ",i+1);
-			auto intr= cast(char[])setIntersection(sys.rows[0].vars,row.vars).array;
+AARow eliminateRowVar(AARow r1,AARow r2) {
+			writefln("looking for intersctions in row %s and %s",r1,r2);
+			auto intr= cast(char[])setIntersection(r1.vars,r2.vars).array;
 			if (intr) {
 				auto var = intr[0];
-					sys.rows[0] = sys.rows[0].mulByLCM(sys.rows[0][var] / row[var]);
-				if (sys.rows[0][var].approxEqual(0)) sys.rows[0].scalars.remove(var);
+					r1 = r1.mulByLCM(r1[var] / r2[var]);
+				if (r1[var].approxEqual(0)) r1.scalars.remove(var);
 			}
-		}
-	}
-	return sys;
+	return r1;
 }
