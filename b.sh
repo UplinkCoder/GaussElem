5ab9a9e9210ca7debc2409a0fed52cb141135670
Stefan K <uplink.coder@gmail.com>
2014-06-28 11:48:31 +0000

    restructure to dub

--- b.sh
+++
@@ -1,3 +0,0 @@
-dmd -O -release GaussParser.d aaSystem.d ctiota.d forops.d mymain.d \
-parseEquationSystem.d reduceMatrix.d pegged/grammar.d pegged/parser.d \
-pegged/peg.d pegged/dynamic/grammar.d pegged/dynamic/peg.d -ofGaussElem
--- build.ldc2.sh
+++
@@ -1,2 +0,0 @@
-gdc *.d pegged/grammar.d pegged/peg.d pegged/parser.d pegged/dynamic/grammar.d pegged/dynamic/peg.d -o- -oq
-
